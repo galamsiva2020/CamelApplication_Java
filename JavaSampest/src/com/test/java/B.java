c670255eead57fbf0ada5c701ad68605a78ca6ea
sivakrishna <sgalam90@gmail.com>
2021-06-05 09:34:47 +0200

    Apache camel java application

---
+++ JavaSampest/src/com/test/java/B.java
@@ -0,0 +1,12 @@
+package com.test.java;
+
+import java.util.*;
+
+public class B  extends Test
+{
+
+		  public static void main(String[] args) {
+		    List<B> listB = new ArrayList<>();
+		    List<B> listA = listB;
+		  }
+}
---
+++ JavaSampest/src/com/test/java/Test.java
@@ -0,0 +1,13 @@
+package com.test.java;
+
+public class Test {
+	String value  ="red";
+	switch (value) {
+	case "red":
+		System.out.println("FAIL");
+	case "green":
+		System.out.println("ok");
+	}
+	
+}
+
